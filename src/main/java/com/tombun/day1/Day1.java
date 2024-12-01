f53ef7429f456f4b747d0ae9b25df0f7d7a5df81
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2024-12-01 09:13:29 +0000

    add day one

--- src/main/java/com/tombun/day1/Day1.java
+++ src/main/java/com/tombun/day1/Day1.java
@@ -1,17 +1,47 @@
 package com.tombun.day1;
 
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-
-import static utils.LoadInput.loadInput;
+import java.util.PriorityQueue;
 
 public class Day1 {
 
-  public static void run() throws IOException {
-    List<String> in = loadInput("/day1/input.txt");
+  public static int partOne(List<String> in) {
+    PriorityQueue<Integer> left = new PriorityQueue<>();
+    PriorityQueue<Integer> right = new PriorityQueue<>();
+
     for (String i : in) {
-      System.out.println(i);
+      String[] lists = i.split(" {3}");
+      left.add(Integer.parseInt(lists[0]));
+      right.add(Integer.parseInt(lists[1]));
     }
+
+    int res = 0;
+
+    for (int i = 0; i < in.size(); i++) {
+      res += Math.abs(left.remove() - right.remove());
+    }
+
+    return res;
   }
 
+  public static int partTwo(List<String> in) {
+    HashMap<Integer, Integer> count = new HashMap<>();
+
+    for (String i : in) {
+      String[] lists = i.split(" {3}");
+      Integer no = Integer.parseInt(lists[1]);
+      count.put(no, count.getOrDefault(no, 0) + 1);
+    }
+
+    int res = 0;
+
+    for (String i : in) {
+      String[] lists = i.split(" {3}");
+      Integer no = Integer.parseInt(lists[0]);
+      res += no * count.getOrDefault(no, 0);
+    }
+
+    return res;
+  }
 }
--- src/test/java/com/tombun/day1/Day1Test.java
+++ src/test/java/com/tombun/day1/Day1Test.java
@@ -4,11 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static utils.LoadInput.loadInput;
+
 class Day1Test {
 
   @Test
-  public void TestDay1() throws IOException {
-    Day1.run();
+  public void TestPartOneExample() throws IOException {
+    int res = Day1.partOne(loadInput("/day1/example.txt"));
+    System.out.println(res);
+  }
+
+  @Test
+  public void TestPartOne() throws IOException {
+    int res = Day1.partOne(loadInput("/day1/input.txt"));
+    System.out.println(res);
+  }
+
+  @Test
+  public void TestPartTwoExample() throws IOException {
+    int res = Day1.partTwo(loadInput("/day1/example.txt"));
+    System.out.println(res);
+  }
+
+  @Test
+  public void TestPartTwo() throws IOException {
+    int res = Day1.partTwo(loadInput("/day1/input.txt"));
+    System.out.println(res);
   }
 
 }
